130cc7c851afcb102f76440bed756517064d5ee4
unknown <hruales1138@gmail.com>
2023-01-11 21:23:07 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001';
-axios.defaults.baseURL = 'https://api-pokemon-production-3f95.up.railway.app/';
+axios.defaults.baseURL = 'https://api-pokemon-production-3f95.up.railway.app';
 
 ReactDOM.render(
   <React.StrictMode>
